1cc6b50e77e6754849063274df26ec59fc972d5f
Rafael Ávila de Espíndola <rafael@espindo.la>
2019-12-27 22:32:22 -0800

    Add tcp related futures
    This adds futures for opening a tcp connection, reading and writing
    to it.
    
    Signed-off-by: Rafael Ávila de Espíndola <rafael@espindo.la>

---
+++ tcp.cc
@@ -0,0 +1,97 @@
+// Copyright (C) 2019 ScyllaDB
+
+#include <cthulhu/reactor.hh>
+#include <cthulhu/tcp.hh>
+
+#include <sys/epoll.h>
+#include <sys/socket.h>
+
+using namespace cthulhu;
+
+tcp_stream::tcp_stream(int fd) : fd(fd) {
+}
+
+connect_future tcp_stream::connect(sockaddr_in addr) {
+	int fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+	if (fd == -1) {
+		return connect_future(posix_error::current());
+	}
+	int r = ::connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+	if (r == -1 && errno != EINPROGRESS) {
+		return connect_future(posix_error::current());
+	}
+	return connect_future(tcp_stream(fd));
+}
+
+read_future tcp_stream::read(void *buf, size_t count) {
+	return read_future(fd, buf, count);
+}
+
+write_future tcp_stream::write(void *buf, size_t count) {
+	return write_future(fd, buf, count);
+}
+
+std::optional<posix_result<tcp_stream>> connect_future::poll(reactor &react) {
+	if (!res) {
+		return std::move(res);
+	}
+	file_descriptor &fd = res->fd;
+
+	// The connection is actually established once it is writable.
+	if (fd.ready_to_write()) {
+		int optval;
+		socklen_t optlen = sizeof(optval);
+		int r = getsockopt(fd.fd, SOL_SOCKET, SO_ERROR, &optval,
+				   &optlen);
+		assert(r == 0);
+		if (optval != 0) {
+			return posix_result<tcp_stream>(posix_error(optval));
+		}
+		return std::move(res);
+	}
+
+	if (posix_error err = react.block_on_write(fd)) {
+		return posix_result<tcp_stream>(std::move(err));
+	}
+
+	return std::nullopt;
+}
+
+connect_future::connect_future(posix_error &&e) : res(std::move(e)) {
+}
+
+connect_future::connect_future(tcp_stream &&s) : res(std::move(s)) {
+}
+
+read_future::read_future(file_descriptor &fd, void *buf, size_t count)
+	: fd(fd), buf(buf), count(count) {
+}
+
+write_future::write_future(file_descriptor &fd, void *buf, size_t count)
+	: fd(fd), buf(buf), count(count) {
+}
+
+static std::optional<posix_result<size_t>>
+io_poll(file_descriptor &fd, ssize_t r, reactor &react,
+	posix_error (reactor::*block_on)(file_descriptor &fd)) {
+	if (r != -1) {
+		return r;
+	}
+	if (errno != EAGAIN) {
+		return posix_error::current();
+	}
+	if (posix_error err = (react.*(block_on))(fd)) {
+		return err;
+	}
+	return std::nullopt;
+}
+
+std::optional<posix_result<size_t>> read_future::poll(reactor &react) {
+	ssize_t r = ::read(fd.fd, buf, count);
+	return io_poll(fd, r, react, &reactor::block_on_read);
+}
+
+std::optional<posix_result<size_t>> write_future::poll(reactor &react) {
+	ssize_t r = write(fd.fd, buf, count);
+	return io_poll(fd, r, react, &reactor::block_on_write);
+}
