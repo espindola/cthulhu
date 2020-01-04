89038dd260625b73d56fb276fb01c8b426a5efc8
Rafael Ávila de Espíndola <rafael@espindo.la>
2020-01-04 12:48:35 -0800

    Enable blocking on IO
    This starts using epoll_wait to block on IO when there are no ready
    tasks to run.
    
    Signed-off-by: Rafael Ávila de Espíndola <rafael@espindo.la>

--- include/cthulhu/reactor.hh
+++ include/cthulhu/reactor.hh
@@ -8,16 +8,26 @@
 #include <cthulhu/transfer_ptr.hh>
 
 namespace cthulhu {
+struct task;
+struct file_descriptor;
+
 class reactor {
 	int epoll_fd;
 	boost::intrusive::list<task> ready;
+	task *current_task;
+	unsigned num_blocked;
+
 	reactor(int epoll_fd);
+	posix_error block_on(file_descriptor &fd, task **t, uint32_t events);
 
 public:
 	reactor(reactor &&react);
 	CTHULHU_EXPORT ~reactor();
 	CTHULHU_EXPORT static posix_result<reactor> create();
 	CTHULHU_EXPORT void add(transfer_ptr<task> tsk);
+
+	posix_error block_on_write(file_descriptor &fd);
+	posix_error block_on_read(file_descriptor &fd);
 
 	template <typename Fut>
 	void add(Fut &&fut) {
--- reactor.cc
+++ reactor.cc
@@ -2,6 +2,8 @@
 
 #include <cthulhu/reactor.hh>
 
+#include <cthulhu/file_descriptor.hh>
+
 #include <sys/epoll.h>
 
 using namespace cthulhu;
@@ -24,16 +26,72 @@ reactor::~reactor() {
 	(void)r;
 }
 
+posix_error reactor::block_on(file_descriptor &fd, task **tsk,
+			      uint32_t events) {
+	assert(current_task);
+	assert(*tsk == nullptr);
+	*tsk = current_task;
+
+	int op = fd.in_epoll ? EPOLL_CTL_MOD : EPOLL_CTL_ADD;
+	fd.in_epoll = true;
+
+	auto i = ready.s_iterator_to(*current_task);
+	ready.erase(i);
+	++num_blocked;
+
+	struct epoll_event event;
+	event.events = events | EPOLLONESHOT;
+	event.data.ptr = &fd;
+
+	// Can fail with ENOMEM or ENOSPC
+	int r = epoll_ctl(epoll_fd, op, fd.fd, &event);
+	if (r != 0) {
+		return posix_error::current();
+	}
+	return posix_error::ok();
+}
+
+posix_error reactor::block_on_write(file_descriptor &fd) {
+	return block_on(fd, &fd.blocked_on_write, EPOLLOUT);
+}
+
+posix_error reactor::block_on_read(file_descriptor &fd) {
+	return block_on(fd, &fd.blocked_on_read, EPOLLIN);
+}
+
 void reactor::run() {
-	for (task &t : ready) {
-		t.poll(*this);
+	for (;;) {
+		for (auto i = ready.begin(); i != ready.end();) {
+			current_task = &*i;
+			++i;
+			current_task->poll(*this);
+		}
+		if (num_blocked == 0) {
+			return;
+		}
+		struct epoll_event event;
+		int num_fds = epoll_wait(epoll_fd, &event, 1, -1);
+		assert(num_fds == 1);
+		auto fd = reinterpret_cast<file_descriptor *>(event.data.ptr);
+		--num_blocked;
+		if (event.events & EPOLLOUT) {
+			ready.push_back(*fd->blocked_on_write);
+			fd->blocked_on_write = nullptr;
+		}
+		if (event.events & EPOLLIN) {
+			ready.push_back(*fd->blocked_on_read);
+			fd->blocked_on_read = nullptr;
+		}
 	}
 }
 
-reactor::reactor(int epoll_fd) : epoll_fd(epoll_fd) {
+reactor::reactor(int epoll_fd)
+	: epoll_fd(epoll_fd), current_task(nullptr), num_blocked(0) {
 }
 
-reactor::reactor(reactor &&o) : epoll_fd(o.epoll_fd) {
+reactor::reactor(reactor &&o)
+	: epoll_fd(o.epoll_fd), ready(std::move(o.ready)),
+	  num_blocked(o.num_blocked) {
 	o.epoll_fd = -1;
 }
 
