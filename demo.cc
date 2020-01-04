0c3bcaeb71ee1c8b544b28bc632944664935cca3
Rafael Ávila de Espíndola <rafael@espindo.la>
2020-01-04 12:51:33 -0800

    Add an echo client demo
    This program opens a tcp connection and sends back anything it
    receives. It is an echo client, instead of the more traditional echo
    server.
    
    It is a nice first program and highlights areas that need some
    usability improvement.
    
    Signed-off-by: Rafael Ávila de Espíndola <rafael@espindo.la>

--- demo.cc
+++ demo.cc
@@ -4,70 +4,97 @@
 #include <cthulhu/either.hh>
 #include <cthulhu/loop.hh>
 #include <cthulhu/reactor.hh>
+#include <cthulhu/tcp.hh>
 
 #include <stdio.h>
 
 using namespace cthulhu;
 
-static auto test_do_with() {
-	return do_with(int(0), [](int &v) {
-		return ready_future_v()
-			.then([&v]() {
-				++v;
-			})
-			.then([&v] {
-				return v;
-			});
+using stop_error = stop_iteration<posix_error>;
+
+static auto write_all(char *buf, tcp_stream &stream, size_t n) {
+	return do_with(std::move(n), [buf, &stream](size_t &n) {
+		return loop([buf, &stream, &n] {
+			return stream.write(buf, n).then(
+				[&n](posix_result<size_t> res) {
+					if (!res) {
+						return stop_error::yes(
+							std::move(res.error()));
+					}
+					n -= *res;
+					if (n == 0) {
+						return stop_error::yes(
+							posix_error::ok());
+					}
+					return stop_error::no();
+				});
+		});
 	});
 }
 
-auto test_loop() {
-	loop l([i = 0]() mutable {
-		++i;
-		printf("in loop %d\n", i);
-		if (i == 3) {
-			return ready_future(stop_iteration_v::yes());
+static auto write_aux(char *buf, tcp_stream &stream, size_t n) {
+	return write_all(buf, stream, n).then([](posix_error e) {
+		if (e) {
+			return stop_error::yes(std::move(e));
 		}
-		return ready_future(stop_iteration_v::no());
+		return stop_error::no();
 	});
-	return l;
 }
 
-static auto get_fut() {
-	return ready_future_v().then([] {
-		printf("get_fut\n");
-		return 41.0;
-	});
+static auto loop_iter(char *buf, tcp_stream &stream, size_t buf_size) {
+	return stream.read(buf, buf_size)
+		.then([&stream, buf](posix_result<size_t> res) {
+			using A = ready_future<stop_error>;
+			using B = decltype(write_aux(buf, stream, *res));
+			using ret_type = either<A, B>;
+			if (!res) {
+				return ret_type(stop_error::yes(
+					std::move(res.error())));
+			}
+			size_t v = *res;
+			if (v == 0) {
+				return ret_type(
+					stop_error::yes(posix_error::ok()));
+			}
+			return ret_type(write_aux(buf, stream, v));
+		});
+}
+
+static auto echo_stream(tcp_stream stream) {
+	using buf_t = std::array<char, 1024>;
+	return do_with(std::make_pair(buf_t(), std::move(stream)),
+		       [](std::pair<buf_t, tcp_stream> &v) {
+			       return loop([&v] {
+				       return loop_iter(v.first.data(),
+							v.second,
+							v.first.size());
+			       });
+		       });
+}
+
+static auto get_tcp_demo() {
+	sockaddr_in addr = {AF_INET, htons(2222), {htonl(INADDR_LOOPBACK)}};
+	return tcp_stream::connect(addr)
+		.then([](posix_result<tcp_stream> stream) {
+			using A = decltype(echo_stream(std::move(*stream)));
+			using B = ready_future<posix_error>;
+			using ret_type = either<A, B>;
+			if (stream) {
+				return ret_type(
+					echo_stream(std::move(*stream)));
+			} else {
+				return ret_type(std::move(stream.error()));
+			}
+		})
+		.then([](posix_error e) {
+			if (e) {
+				e.print_error(stderr);
+				fprintf(stderr, "\n");
+			}
+		});
 }
 
 int main(int argc, const char *argv[]) {
-	auto fut = ready_future_v()
-			   .then([a = std::make_unique<int>(42)]() {
-				   printf("foo\n");
-				   return std::make_unique<int>(42);
-			   })
-			   .then([argc](std::unique_ptr<int> x) {
-				   using A = ready_future<double>;
-				   using B = decltype(get_fut());
-				   using ret_type = either<A, B>;
-
-				   printf("bar\n");
-				   if (argc > 1) {
-					   return ret_type(double(*x));
-				   }
-				   return ret_type(get_fut());
-			   })
-			   .then([](double y) {
-				   printf("zed %f\n", y);
-				   return std::make_unique<int>(41);
-			   })
-			   .then([](std::unique_ptr<int> y) {
-			   });
-
-	auto fut2 = test_do_with().then([](int v) {
-		printf("do_with test %d\n", v);
-	});
-
 	posix_result<reactor> react_res = reactor::create();
 	if (!react_res) {
 		react_res.error().print_error(stderr);
@@ -75,10 +102,8 @@ int main(int argc, const char *argv[]) {
 	}
 	reactor &react = *react_res;
 
-	react.add(std::move(fut));
-
-	react.add(test_loop());
-	react.add(std::move(fut2));
+	react.add(get_tcp_demo());
 	react.run();
+
 	return 0;
 }
