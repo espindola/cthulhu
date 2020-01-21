659263d12ea5d47f1d08d4c5a02d948890d90f2b
Rafael Ávila de Espíndola <rafael@espindo.la>
2020-01-20 22:42:40 -0800

    Use then_helper in and_then
    Now the lambda passed to and_then can return a plain value in addition
    to a future and if the output of the current future is monostate, the
    lambda takes no arguments instead of a monostate argument.
    
    Signed-off-by: Rafael Ávila de Espíndola <rafael@espindo.la>

--- include/cthulhu/and_then.hh
+++ include/cthulhu/and_then.hh
@@ -8,12 +8,15 @@ namespace cthulhu {
 template <typename Self>
 template <typename F>
 auto future<Self>::and_then(F &&f) {
-	return then([f = std::move(f)](typename Self::output v) {
-		using A = decltype(f(std::move(*v)));
+	using helper =
+		internal::then_helper<typename Self::output::value_type, F>;
+
+	return then([f = std::move(f)](typename Self::output v) mutable {
+		using A = typename helper::type;
 		using B = ready_future<typename A::output>;
 		using ret_type = either<A, B>;
 		if (v) {
-			return ret_type(f(std::move(*v)));
+			return ret_type(helper::apply(f, std::move(*v)));
 		}
 		return ret_type(B(v.error()));
 	});
